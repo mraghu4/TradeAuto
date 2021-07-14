772b175979c4a1a0f0317faf917d937bf7e8c6c5
ermeagn <raghu.menampalli@ericsson.com>
2021-07-14 14:34:39 +0000

    Added code for session init which ask token only once per day

--- start_trade.py
+++ start_trade.py
@@ -1,4 +1,8 @@
+import re
+import datetime
 import logging
+import datetime
+from pathlib import Path
 from kiteconnect import KiteConnect
 from inputs.input_parser import InputParser
 
@@ -12,6 +16,7 @@ ip = InputParser()
 API_KEY = ip.get_apikey()
 API_SECRET = ip.get_apisecret()
 kite = KiteConnect(api_key=API_KEY)
+TOKEN_FILE = "/tmp/kite_token" 
 
 
 def get_access_token():
@@ -20,14 +25,31 @@ def get_access_token():
 
 
 def record_login(access_token):
-#TODO
-def last_login():
-#TODO
+    time_now = datetime.date.today()
+    fd = open(TOKEN_FILE,"w")
+    fd.write("{}:{}".format(time_now,access_token))
+
+def days_from_last_login():
+    token_file = Path(TOKEN_FILE)
+    if not token_file.exists():
+       #never logged in need a new session
+       return 1
+    fd = open(TOKEN_FILE,"r")
+    data = fd.read()
+    pre_date = re.search("([\w\-]+)\:",data).group(1)
+    y,m,d = map(int,pre_date.split("-"))
+    pre_date = datetime.date(y,m,d)
+    date_now = datetime.date.today()
+    return (date_now-pre_date).days
+
 def get_last_access_token():
-#TODO
+    fd = open(TOKEN_FILE,"r")
+    data = fd.read()
+    return re.search("\:(\w+)",data).group(1)
+
 
 def generate_session():
-    if last_login() > 24: 
+    if days_from_last_login() > 0: 
        #generate new token if not logged in last 24 Hours
        data = kite.generate_session(get_access_token(), api_secret=API_SECRET)
        access_token = data["access_token"]
@@ -36,12 +58,11 @@ def generate_session():
        #use same access if logged in last 24 Hours
        access_token = get_last_access_token()
 
-    kite.set_access_token(data["access_token"])
+    kite.set_access_token(access_token)
 
 
 def main():
     generate_session()
-    
     
         
 
