3ffe5bfbd6d14fd20d1a377798fabbe9cbe20a19
mraghu4 <menampalli.raghu@gmail.com>
2021-07-18 23:15:00 +0530

    Started implementing intraday strategy

--- strategies/options/intraday_stradel/opt_intraday_stradel_strategy.py
+++ strategies/options/intraday_stradel/opt_intraday_stradel_strategy.py
@@ -1,4 +1,6 @@
 import logging
+from datetime import datetime
+import strategies.exitcodes as exitcode
 
 """
    Description:
@@ -10,8 +12,46 @@ import logging
 class IntradayStradel():
       kite = None
       instrument = None
+      inputs = None
+
+      def print_description():
+          logging.info(self.inputs.strategy.description)
+
+      def wait_till_time(self,entry_time):
+          while True:
+              if (datetime.now().time() >=
+                      datetime.strptime(entry_time,'%H:%M').time()):
+                  return
+              time.sleep(60)
+
+      def check_exit_time(self,exit_time):
+          if (datetime.now().time() >=
+                  datetime.strptime(exit_time,'%H:%M').time()):
+              logging.info("Current time beyond stragtegy exit time")
+              exit(exitcode.EXIT_TIME_TRIGGER)
+          else:
+              return False
+
+      def trade_stradel():
+          #TODO
+          return None
+
+      def watch_adjust_or_exit():
+          #TODO
+          return None
+
+
+      def execute_strategy(self):
+          if self.inputs.strategy.entry.type == "time":
+              self.check_exit_time(self.inputs.strategy.exit.time)
+              self.wait_till_time(self.inputs.strategy.entry.time)
+          self.trade_stradel()
+          self.watch_adjust_or_exit()
 
       def start_trade(self,kite,inputs):
           self.kite = kite
-          logging.info(inputs.strategy.description)
+          self.inputs = inputs
+          self.execute_strategy() 
+
+
 
