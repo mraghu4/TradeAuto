27fb2477f6e6d10aef30e2d8fd4d9d30c828f79d
Suresh Ganta <suresh.komarada@gmail.com>
2021-07-19 00:01:31 +0530

    Update start_trade.py

--- start_trade.py
+++ start_trade.py
@@ -10,7 +10,9 @@ from inputs.input_parser import InputParser
 
 
 logging.basicConfig(level=logging.DEBUG)
-
+"""
+ You might want to get debug mode from cmd line args when program starts. you dont want to run in debug mode always.
+"""
 
 
 ip = InputParser()
@@ -18,24 +20,33 @@ API_KEY = ip.get_apikey()
 API_SECRET = ip.get_apisecret()
 kite = KiteConnect(api_key=API_KEY)
 TOKEN_FILE = "/tmp/kite_token" 
+# File seperator might vary from OS to OS, you might use file seperator from OS module.
 
 
 def get_access_token():
     logging.info("Copy {} , login and"
        " provide access token".format(kite.login_url()))
     return input("Enter Token:").strip()
+    # You might want to accept this from command line instead making interactive
 
 
 def record_login(access_token):
     time_now = datetime.date.today()
     fd = open(TOKEN_FILE,"w")
+    # Consider using 'when open(TOKEN_FILE,"w") as fd':
+    # Or, Null check for fd, close the fd after done.
     fd.write("{}:{}".format(time_now,access_token))
+    # fd.write(f"{}:{}", time_now,access_token) is short hand notation for format.
+    
 
 def get_last_login_date():
     fd = open(TOKEN_FILE,"r")
+    # Consider using 'when open(TOKEN_FILE,"w") as fd':
+    # Or, Null check for fd, close the fd after done.
     data = fd.read()
     pre_date = re.search("([\w\-]+)\:",data).group(1)
     y,m,d = map(int,pre_date.split("-"))
+    # string to date methods are available in Python, you just need to specify what is the format of the input string whether dd-mm-yyyy or mm-dd-yyyy for example.
     return  datetime.date(y,m,d)
 
 
@@ -44,6 +55,8 @@ def days_from_last_login():
     if not token_file.exists():
        #never logged in need a new session
        return 1
+       # but you're checking for >0 .
+       # You can return 'None' alternatively.
     pre_date = get_last_login_date()
     date_now = datetime.date.today()
     return (date_now-pre_date).days
@@ -51,12 +64,15 @@ def days_from_last_login():
 def get_last_access_token():
     try:
        fd = open(TOKEN_FILE,"r")
+       # Consider using 'when open(TOKEN_FILE,"w") as fd':
+       # Or, Null check for fd, close the fd after done.
        data = fd.read()
        return re.search("\:(\w+)",data).group(1)
-    except  e:
+     
+    except e:
        logging.info("{} file might be deleted or modified.\n"
             "please do \"rm -f {}\" it and rerun".format(TOKEN_FILE,TOKEN_FILE)) 
-
+        # this can of logger.error category.
 
 def generate_session():
     if days_from_last_login() > 0: 
@@ -65,6 +81,7 @@ def generate_session():
            data = kite.generate_session(get_access_token(), api_secret=API_SECRET)
        except e:
            logging.info("Not able to connect, Check input params and token passed")
+            # can be error category.
        access_token = data["access_token"]
        record_login(access_token)
     else:
